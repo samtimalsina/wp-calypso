866db5cb04e88bc9b44c9f459e8ecbf67247a3d6
Allen Snook <allen@allendav.com>
2017-05-03 16:37:03 -0700

    Use a data driven approach to register all store routes needed by nav

--- client/extensions/woocommerce/index.js
+++ client/extensions/woocommerce/index.js
@@ -14,31 +14,84 @@ import ProductCreate from './app/products/product-create';
 import Dashboard from './app/dashboard';
 import StatsController from './app/stats/controller';
 
-const Controller = {
-	dashboard: function( context ) {
-		renderWithReduxStore(
-			React.createElement( Dashboard, { } ),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	},
-
-	addProduct: function( context ) {
-		renderWithReduxStore(
-			React.createElement( ProductCreate, { } ),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	}
-};
-
 export default function() {
-	if ( config.isEnabled( 'woocommerce/extension-dashboard' ) ) {
-		page( '/store/:site', siteSelection, navigation, Controller.dashboard );
-		page( '/store/products/:site/add', siteSelection, navigation, Controller.addProduct );
-	}
+	const storePages = [
+		{
+			container: Dashboard,
+			configKey: 'woocommerce/extension-dashboard',
+			route: '/store/:site',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-products',
+			route: '/store/products/:site',
+		},
+		{
+			container: ProductCreate,
+			configKey: 'woocommerce/extension-products',
+			route: '/store/products/:site/add',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-products-import',
+			route: '/store/products/:site/import',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-orders',
+			route: '/store/orders/:site',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-orders',
+			route: '/store/orders/:site/add',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-promotions',
+			route: '/store/promotions/:site',
+		},
+		{
+			container: StatsController,
+			configKey: 'woocommerce/extension-stats',
+			route: '/store/stats/:type/:period/:site',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-extensions',
+			route: '/store/extensions/:site',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-settings',
+			route: '/store/settings/:site',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-settings-checkout',
+			route: '/store/settings/:site/checkout',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-settings-shipping',
+			route: '/store/settings/:site/shipping',
+		},
+		{
+			container: Dashboard, // TODO use Dashboard as a placeholder until this page becomes available
+			configKey: 'woocommerce/extension-settings-tax',
+			route: '/store/settings/:site/tax',
+		},
+	];
 
-	if ( config.isEnabled( 'woocommerce/extension-stats' ) ) {
-		page( '/store/stats/:type/:period/:site', siteSelection, navigation, StatsController );
-	}
+	storePages.forEach( function( storePage ) {
+		if ( config.isEnabled( storePage.configKey ) ) {
+			page( storePage.route, siteSelection, navigation, function( context ) {
+				renderWithReduxStore(
+					React.createElement( storePage.container, { } ),
+					document.getElementById( 'primary' ),
+					context.store
+				);
+			} );
+		}
+	} );
 }
